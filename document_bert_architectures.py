4a93d0a971e9c4cce90d3ad362f3bcb40f5e4532
Xuanyang Lin <75073738+xxlin-haa@users.noreply.github.com>
2022-06-06 22:09:33 -0400

    Update document_bert_architectures.py

--- document_bert_architectures.py
+++ document_bert_architectures.py
@@ -35,12 +35,17 @@ class DocumentBertSentenceChunkAttentionLSTM(BertPreTrainedModel):
                                         min(document_batch.shape[1],
                                             bert_batch_size),
                                         self.bert.config.hidden_size), dtype=torch.float, device=device)
+        
+        # One iteration works on one document only (not entire batch).
         for doc_id in range(document_batch.shape[0]):
+            # apply BERT attention layers followed by dropout
             bert_output[doc_id][:bert_batch_size] = self.dropout(self.bert(document_batch[doc_id][:bert_batch_size,0],
-                                                                           token_type_ids=document_batch[doc_id][:bert_batch_size, 1],
+                                                                           token_type_ids=document_batch[doc_id][:bert_batch_size, 1], 
                                                                            attention_mask=document_batch[doc_id][:bert_batch_size, 2])[1])
+        # LSTM
         output, (_, _) = self.lstm(bert_output.permute(1, 0, 2))
         output = output.permute(1, 0, 2)
+        # the attention layer following LSTM 
         # (batch_size, seq_len, num_hiddens)
         attention_w = torch.tanh(torch.matmul(output, self.w_omega) + self.b_omega)
         attention_u = torch.matmul(attention_w, self.u_omega)  # (batch_size, seq_len, 1)
@@ -70,11 +75,14 @@ class DocumentBertCombineWordDocumentLinear(BertPreTrainedModel):
                                         min(document_batch.shape[1], self.bert_batch_size),
                                         self.bert.config.hidden_size * 2),
                                   dtype=torch.float, device=device)
+        # one iter works on one example (not entire batch)
         for doc_id in range(document_batch.shape[0]):
             all_bert_output_info = self.bert(document_batch[doc_id][:self.bert_batch_size,0],
                                              token_type_ids=document_batch[doc_id][:self.bert_batch_size, 1],
                                              attention_mask=document_batch[doc_id][:self.bert_batch_size, 2])
+            # max pooling for all token-scale representations
             bert_token_max = torch.max(all_bert_output_info[0], 1)
+            # concat token-scale and document-scale representations
             bert_output[doc_id][:self.bert_batch_size] = torch.cat((bert_token_max.values, all_bert_output_info[1]), 1)
 
         prediction = self.mlp(bert_output.view(bert_output.shape[0], -1))
